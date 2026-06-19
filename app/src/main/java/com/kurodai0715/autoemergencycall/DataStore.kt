494a3efb0f32f8ad857d38121c7dfe481caf209f
奥田 智紘 <okuda0715tech@gmail.com>
2026-06-19 22:42:56 +0900

    feat: プロジェクト新規作成

---
+++ app/src/main/java/com/kurodai0715/autoemergencycall/DataStore.kt
@@ -0,0 +1,26 @@
+package com.kurodai0715.autoemergencycall
+
+import android.content.Context
+import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+
+private const val DATASTORE_NAME = "heartbeat"
+
+val Context.dataStore by preferencesDataStore(
+    name = DATASTORE_NAME
+)
+
+object PreferenceKeys {
+    val LAST_CHARGING_STARTED_AT =
+        longPreferencesKey("last_charging_started_at")
+}
+
+suspend fun Context.saveChargingStartedAt(
+    timestamp: Long
+) {
+    dataStore.edit { preferences ->
+        preferences[PreferenceKeys.LAST_CHARGING_STARTED_AT] =
+            timestamp
+    }
+}
---
+++ app/src/main/java/com/kurodai0715/autoemergencycall/HeartbeatType.kt
@@ -0,0 +1,9 @@
+package com.kurodai0715.autoemergencycall
+
+enum class HeartbeatType {
+    CHARGING_STARTED,
+    SCREEN_UNLOCKED,
+    APP_OPENED,
+    STEP_DETECTED,
+    MANUAL_CHECKIN
+}
