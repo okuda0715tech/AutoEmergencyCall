cf5fbfa9db796bd59270ac6384e4d02b36742d33
奥田 智紘 <okuda0715tech@gmail.com>
2026-06-25 15:07:17 +0900

    build: バージョン情報の更新
    - `versionCode` を 2 に更新
    - `versionName` を 1.1.0 に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.kurodai0715.autoemergencycall"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
