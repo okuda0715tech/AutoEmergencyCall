6df1075b54c847e6a483ea27f4044a5064af90eb
奥田 智紘 <okuda0715tech@gmail.com>
2026-07-19 23:39:40 +0900

    chore: アプリバージョンを 1.6.0 に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "com.kurodai0715.autoemergencycall"
         minSdk = 30
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
